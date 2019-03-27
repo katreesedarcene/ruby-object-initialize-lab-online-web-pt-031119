b7294e6c43b11434a7580b805fc1c934d784231a
Kat B <katreesebarnes@gmail.com>
2019-03-27 23:24:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog 
-   def initialize(name, breed)
+   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
@@ -10,7 +10,7 @@ class Dog
     @name
   end
   def breed=(breed)
-    @breed = "Mutt"
+    @breed = breed
   end
   def breed 
     @breed
